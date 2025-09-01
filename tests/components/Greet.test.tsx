af69fd3702af7d42d62307dd6a9e59c5d1e63504
Alexandria <alexandriamlewis@me.com>
2025-09-01 13:59:09 -0400

    tests with the imports

---
+++ tests/components/Greet.test.tsx
@@ -0,0 +1,22 @@
+import "@testing-library/jest-dom/vitest";
+import { render, screen } from "@testing-library/react";
+import { describe, expect, it } from "vitest";
+import Greet from "../../src/components/Greet";
+
+describe("group", () => {
+  it("should render Hello with the name when name is provided", () => {
+    render(<Greet name="Mosh" />);
+
+    const heading = screen.getByRole("heading");
+    expect(heading).toBeInTheDocument();
+    expect(heading).toHaveTextContent(/mosh/i);
+  });
+
+  it("should render login button when name is not provided", () => {
+    render(<Greet />);
+
+    const button = screen.getByRole("button");
+    expect(button).toBeInTheDocument();
+    expect(button).toHaveTextContent(/login/i);
+  });
+});
