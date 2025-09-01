be90c090ac92cc50fef551ede0fa153734e4e95c
Alexandria <alexandriamlewis@me.com>
2025-09-01 16:54:53 -0400

    testing truncating text

--- src/pages/PlaygroundPage.tsx
+++ src/pages/PlaygroundPage.tsx
@@ -1,7 +1,9 @@
-import Onboarding from "../components/Onboarding";
+import ExpandableText from "../components/ExpandableText";
 
 const PlaygroundPage = () => {
-  return <Onboarding />;
+  return (
+    <ExpandableText text="Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptatum minima nostrum nam fuga, distinctio quo ab dicta maxime illum error a odit qui explicabo, porro excepturi. Aliquid dolores distinctio quos provident expedita eligendi eos sunt, at corrupti iure repellendus, voluptatibus facilis quo, commodi dolorem obcaecati in tempora maxime sequi amet? Quasi et sint corrupti. Dignissimos nulla voluptas mollitia ut quaerat repudiandae itaque corrupti modi, eveniet expedita voluptate doloremque, rerum accusantium illo minus nobis perspiciatis ab! Quis est ipsa eum molestiae illum praesentium. Perferendis nostrum mollitia perspiciatis in sapiente, nam ullam fuga laboriosam suscipit et iure est modi. Aspernatur, incidunt eius!" />
+  );
 };
 
 export default PlaygroundPage;
